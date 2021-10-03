cf7ca4aea33d05d5bd4ea4f671c3c55649914105
Niket Malik <niketmalik@gmail.com>
2021-10-03 21:45:51 +0530

    Fixing toolbar position

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -4,14 +4,18 @@ import { Hero } from "components/hero";
 import { Board } from "components/board";
 import { Toolbar } from "components/toolbar";
 
+import { AppContainer } from "./styles";
+
 import "static/css/styles.css";
 
 export const App = () => {
   return (
-    <div className="p-5 py-3 py-md-5" data-testid="app">
-      <Hero />
-      <Board />
+    <>
+      <AppContainer className="p-5 py-3 py-md-5" data-testid="app">
+        <Hero />
+        <Board />
+      </AppContainer>
       <Toolbar />
-    </div>
+    </>
   );
 };
